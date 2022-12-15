071603458773c938937c925a1f432df087235fa4
Spencer Miskoviak <smiskoviak@gmail.com>
2022-12-15 15:20:55 -0700

    improve mobile layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,10 @@ export default function Document() {
           rel="stylesheet"
           href="//fonts.googleapis.com/css?family=Google+Sans:400,500,700"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <body>
         <Main />
