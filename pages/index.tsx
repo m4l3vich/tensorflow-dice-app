ae60335dd736bb1f624e3bded873f70cddcf1e27
Spencer Miskoviak <smiskoviak@gmail.com>
2023-01-11 20:15:28 -0700

    add github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { WebCam } from "../components/web-cam";
 import styles from "../styles/Home.module.css";
 
@@ -11,6 +12,14 @@ export default function Home() {
 
       <nav className={styles.nav}>
         <h1>🎲 Dice tracker</h1>
+        <a
+          href="https://github.com/skovy/tensorflow-dice-app"
+          target="_blank"
+          className={styles.navLink}
+        >
+          <Image src="/github-mark.svg" width={16} height={16} alt="" />
+          <span>GitHub</span>
+        </a>
       </nav>
 
       <main className={styles.main}>
